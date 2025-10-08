fed40da93cc93e0349b3316be585c0d5078e14d9
marlinshenoda <marlin.shenoda.java25@edu.edugrade.se>
2025-10-08 20:57:56 +0200

    entity student

---
+++ StudentManagerApp/Data1/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagerApp.Data1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; 
+        public double Grade { get; set; }
+    }
+}
