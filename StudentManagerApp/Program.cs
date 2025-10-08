a1a7d7758a35b635942bf555a2bfc3ecb06078cb
marlinshenoda <marlin.shenoda.java25@edu.edugrade.se>
2025-10-08 21:38:05 +0200

    student view

--- StudentManagerApp/Program.cs
+++ StudentManagerApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StudentManagerApp;
+using StudentManagerApp.Data1.Repository;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IStudentService, StudentService>();
 
 await builder.Build().RunAsync();
