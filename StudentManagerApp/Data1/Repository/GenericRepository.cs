7a403a287b7e32897d80ec87531cec8df5207a21
marlinshenoda <marlin.shenoda.java25@edu.edugrade.se>
2025-10-08 21:24:53 +0200

    REPOSITORY

---
+++ StudentManagerApp/Data1/Repository/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿
+namespace StudentManagerApp.Data1.Repository
+{
+    public class StudentService : IStudentService
+    {
+        private List<Student> _students = new();
+        public List<Student> GetStudents() => _students;
+        private int _nextId = 1;
+
+
+        public void AddStudent(Student student)
+        {
+            student.Id = _nextId++;
+
+            _students.Add(student);
+        }
+
+        public void DeleteStudent(string name)
+        {
+            _students.RemoveAll(s => s.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase));
+        }
+
+
+
+        public void SortByGrade()
+        {
+            _students = _students.OrderByDescending(s => s.Grade).ToList();
+        }
+
+        public void SortByName()
+        {
+            _students = _students.OrderBy(s => s.Name).ToList();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            var existingStudent = _students.FirstOrDefault(s => s.Id == student.Id);
+            if (existingStudent != null)
+            {
+                existingStudent.Name = student.Name;
+                existingStudent.Grade = student.Grade;
+            }
+        }
+    }
+}
---
+++ StudentManagerApp/Data1/Repository/IStudentService.cs
@@ -0,0 +1,20 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StudentManagerApp.Data1.Repository
+{
+    public interface  IStudentService
+    {
+        List<Student> GetStudents();
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void DeleteStudent(string student);
+        void SortByName();
+        void SortByGrade();
+
+    }
+}
